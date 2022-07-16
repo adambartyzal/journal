cba3a822ec96ff90a41f6d764ef2093c069106fc
adambartyzal <adambartyzal@gmail.com>
2022-07-16 08:55:22 +0200

    publish script generates md files from raw files

--- publish.sh
+++ publish.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+echo Converting raw files to markdown...
+for file in *.raw; do
+  python replace.py $file
+done
+echo Done
 
-echo Generating thumbnails
+echo Generating thumbnails...
 cd images
 ./makethumbnails.sh
 cd ..
 echo Done
+
 git stage *
 git status
 read -p "Enter commit message: " message
