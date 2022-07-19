2086cb1dd62cf081ee2bd9826b2fae78c363e23a
adambartyzal <adambartyzal@gmail.com>
2022-07-19 23:05:09 +0200

    test if publish is necessary

--- publish.sh
+++ publish.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
-echo Converting raw files to markdown...
-for file in *.raw; do
-  python replace.py $file
-done
-echo Done
-
-echo Generating thumbnails...
-cd images
-./makethumbnails.sh
-cd ..
-echo Done
-
-git stage *
-git status
-read -p "Enter commit message: " message
-git commit -m "$message"
-
-if connmanctl state | grep -q "  State = online"; then
-  git push
+if git status | grep -q "nothing to commit"; then
+  echo Nothing to publish.
 else
-  echo Offline, not pushing to remote.
-fi
+  echo Converting raw files to markdown...
+  for file in *.raw; do
+    python replace.py $file
+  done
+  echo Done
 
-echo Publishing finished.
+  echo Generating thumbnails...
+  cd images
+  ./makethumbnails.sh
+  cd ..
+  echo Done
+
+  git stage *
+  git status
+  read -p "Enter commit message: " message
+  git commit -m "$message"
+
+  if connmanctl state | grep -q "State = online"; then
+    git push
+  else
+    echo Offline, not pushing to remote.
+  fi
+
+  echo Publishing finished.
+fi
