b9715ad2c7bd337c44b44c0d805fd0470d33ef28
adambartyzal <adambartyzal@gmail.com>
2022-07-25 13:14:04 +0200

    newthumbnails script generates only added or modified images

--- newthumbnails.sh
+++ newthumbnails.sh
@@ -3,11 +3,13 @@
 files=$(git status --porcelain)
 for file in $files; do
   if [[ $file == *.jpg ]]; then
-    dir=$(dirname $file)
-    if ! test -f "images/thumbnails/$dir"; then
-      mkdir -p "images/thumbnails/$dir"
+    if test -f $file; then
+      dir=$(dirname $file)
+      if ! test -f "images/thumbnails/$dir"; then
+        mkdir -p "images/thumbnails/$dir"
+      fi
+      dest=${file:7}
+      convert "$file" -resize 700 -quality 50 "images/thumbnails/$dest"
     fi
-    dest=${file:7}
-    convert "$file" -resize 700 -quality 50 "images/thumbnails/$dest"
   fi
 done
