d7af98bd1d315e2c8682c22ce1e43e91dd932443
adambartyzal <adambartyzal@gmail.com>
2022-07-25 13:07:38 +0200

    fix

--- newthumbnails.sh
+++ newthumbnails.sh
@@ -4,8 +4,8 @@ files=$(git status --porcelain)
 for file in $files; do
   if [[ $file == *.jpg ]]; then
     dir=$(dirname $file)
-    if ! test -f "thumbnails/$dir"; then
-      mkdir -p "thumbnails/$dir"
+    if ! test -f "images/thumbnails/$dir"; then
+      mkdir -p "images/thumbnails/$dir"
     fi
     dest=${file:7}
     convert "$file" -resize 700 -quality 50 "images/thumbnails/$dest"
